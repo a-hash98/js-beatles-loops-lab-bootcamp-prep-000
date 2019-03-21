0abae3655db182586fcb0373f87421400514525c
Ay Has <ahashim983@yahoo.com>
2019-03-21 21:07:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 function theBeatlesPlay(musicians, instruments){
   var musiciansAndInstruments = []
+<<<<<<< HEAD
   for (var i = 0; i < musicians.length; i++){
     var sentence = musicians[i] + " plays " + instruments[i]
+=======
+  for (i = 0; i < musicians.length; i++){
+    var sentence = musicians[i] + " plays " instruments[i]
+>>>>>>> 7c174e879e1f815a578abd91b5c5860cb11dcc03
     musiciansAndInstruments.push(sentence)
   }
   return musiciansAndInstruments
@@ -9,7 +14,11 @@ function theBeatlesPlay(musicians, instruments){
 
 function johnLennonFacts(jLFacts){
   var i = 0
+<<<<<<< HEAD
   while (jLFacts[i]){
+=======
+  while (jLFact[i]){
+>>>>>>> 7c174e879e1f815a578abd91b5c5860cb11dcc03
     jLFacts[i] = jLFacts[i].concat("!!!")
     i++
   }
